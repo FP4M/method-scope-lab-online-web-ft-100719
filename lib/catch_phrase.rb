cd8ef1eb2f2b0d785dcea450c79eda6ccb819b23
Prince Charles <princecharles35@yahoo.com>
2019-11-08 15:13:14 +0000

    Automatically backed up by Learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -3,4 +3,4 @@ the_best_or_nothing = "Mercedes Benz"
   def catch_phrase(the_best_or_nothing)
      puts "#{the_best_or_nothing} are my faviort cars of all time."
    end
-   
+   def catch_phrase(the_best_or_nothing)
